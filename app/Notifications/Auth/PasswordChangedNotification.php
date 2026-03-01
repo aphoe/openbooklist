0afcd2dda3e033b6bd3acca24e22d08be7cecacc
afolabi legunsen <aphoe@outlook.com>
2026-03-01 16:55:29 +0100

    Move DashboardController to Users namespace
    Rename DashboardController into app/Http/Controllers/Users and update its namespace to App\Http\Controllers\Users. Add a use statement for App\Http\Controllers\Controller so the class can extend the base controller from the new namespace.

---
+++ app/Notifications/Auth/PasswordChangedNotification.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Notifications\Auth;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class PasswordChangedNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('Your Password Has Been Changed')
+            ->greeting('Hello!')
+            ->line('This is a confirmation that the password for your account has just been changed.')
+            ->line('If this was you, you can safely ignore this email.')
+            ->line('If you did not request a password change, please contact support immediately.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
